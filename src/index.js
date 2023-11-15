bd26b39093a12b30f1cda7be8a598b38b29b4081
terrason <jterraghost@gmail.com>
2023-11-15 20:41:42 +0800

    修复相对路径

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/resume">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
