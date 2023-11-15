68d67911ab242ccf0b1b4d1ed4385acfa35266a8
terrason <jterraghost@gmail.com>
2023-11-15 17:25:37 +0800

    修复相对路径

--- script/deploy.sh
+++ script/deploy.sh
@@ -18,9 +18,10 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:terrason/terrason.github.io.git gh-pages
+# git push -f git@github.com:terrason/terrason.github.io.git gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:terrason/resume.git main:gh-pages
 
 cd -
